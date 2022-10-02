9ae40370ee15e4bf9db296d04beaa4d641c742f9
Asmit <asmitbuddy@gmail.com>
2022-10-02 15:16:19 +0530

    Time: 100 ms (21.28%), Space: 8.4 MB (45.97%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -14,11 +14,10 @@ public:
             count=(count%mod + rec(n-1,k,target-i)%mod)%mod;
             dp[n][target]=count;
         }
-        
         return count;
     }
     int numRollsToTarget(int n, int k, int target) {
-        dp.resize(31,vector<int> (1001,-1));
+        dp.resize(n+1,vector<int> (target+1,-1));
         return rec(n,k,target);
     }
 };
